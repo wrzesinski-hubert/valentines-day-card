dbdbef039ea42bede3e56f3694105377236562c3
wrzesinski-hubert <wrzesinski.hubert@gmail.com>
2025-01-20 20:49:23 +0100

    create card

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import Cosmo from "./assets/Cosmo.png";
 import Daffodil from "./assets/Daffodil.png";
 import Daisy from "./assets/Daisy.png";
@@ -11,11 +11,14 @@ import Poppy from "./assets/Poppy.png";
 import Rose from "./assets/Rose.png";
 import Sunflower from "./assets/Sunflower.png";
 import Tulip from "./assets/Tulip.png";
-import "./App.css";
+import Ja from "./assets/1.jpeg";
+import "./App.scss";
+
+const NUMBER_OF_TULIPS = 5;
 
 const generateRandomNumbers = (range: number) => {
   const randomNumbers: number[] = [];
-  while (randomNumbers.length < 5) {
+  while (randomNumbers.length < NUMBER_OF_TULIPS) {
     const randomNumber = Math.floor(Math.random() * range) + 1;
     if (!randomNumbers.includes(randomNumber)) {
       randomNumbers.push(randomNumber);
@@ -38,9 +41,10 @@ export const App = () => {
     Rose,
     Sunflower,
   ];
+
   const [difficultyLevel, setDifficultyLevel] = useState(1);
-  const [takenFlowers, setTakenFlowers] = useState<number[]>([]);
-  const ref = useRef<HTMLDivElement>(null);
+  const [clickedFlowers, setClickedFlowers] = useState<number[]>([]);
+  const listOfLetters = ["K", "O", "C", "H", "A", "M"];
 
   const listOfAllFlowers = useMemo(
     () =>
@@ -49,66 +53,123 @@ export const App = () => {
       ),
     [difficultyLevel]
   );
+
   const randomPlacesForTulips = useMemo(
     () => generateRandomNumbers(100 * difficultyLevel),
     [difficultyLevel]
   );
 
   useEffect(() => {
-    setTakenFlowers([]);
+    setClickedFlowers([]);
   }, [difficultyLevel]);
 
+  const handleFlowerClick = (index: number) => {
+    if (!clickedFlowers.includes(index)) {
+      setClickedFlowers((prev) => [...prev, index]);
+    }
+  };
+
   return (
-    <div className="flowers-wrapper">
-      <div className="difficulty-wrapper">
-        <div className="difficulty" onClick={() => setDifficultyLevel(1)}>
-          Easy
-        </div>
-        <div className="difficulty" onClick={() => setDifficultyLevel(2)}>
-          Medium
-        </div>
-        <div className="difficulty" onClick={() => setDifficultyLevel(3)}>
-          Hard
-        </div>
-      </div>
-      <div
-        className="flowers-grid"
-        style={{ gridTemplateColumns: `repeat(${10 * difficultyLevel},25px)` }}
-        ref={ref}
-      >
-        {listOfAllFlowers.map((flower, index) => (
-          <div
-            key={index}
-            className={
-              "single-flower-wrapper " +
-              (takenFlowers.includes(index) ? "single-flower-taken" : "")
-            }
-            style={
-              takenFlowers.includes(index)
-                ? {
-                    transform: "translateY(150px)",
-                  }
-                : {}
-            }
-          >
-            <img
-              src={
-                randomPlacesForTulips.includes(index)
-                  ? Tulip
-                  : arrayOfFlowers[flower]
-              }
-              onClick={() => {
-                setTakenFlowers((prev) => [...prev, index]);
-                console.log(
-                  ref.current?.offsetTop,
-                  ref.current?.getClientRects()
-                );
-              }}
-              alt="flower"
-            />
+    <>
+      {clickedFlowers.length === NUMBER_OF_TULIPS && (
+        <div className="container-absolute">
+          <div className="container-relative">
+            {Array.from({ length: 200 }, (_, index) => (
+              <div className="circle-container">
+                <div className="circle" />
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-    </div>
+        </div>
+      )}
+      <>
+        {clickedFlowers.length === NUMBER_OF_TULIPS ? (
+          <>
+            <div className="flowers-wrapper">
+              <img src={Ja} width={400} />
+              <p className="message">Will u be my valentine? 🥺</p>
+            </div>
+          </>
+        ) : (
+          <div className="flowers-wrapper">
+            <div className="title">
+              Find all {NUMBER_OF_TULIPS - clickedFlowers.length} tulips{" "}
+              <img src={Tulip} />
+            </div>
+
+            <div className="difficulty-wrapper">
+              <div
+                className={
+                  "difficulty-button " +
+                  (difficultyLevel === 1 ? "selected" : "")
+                }
+                onClick={() => setDifficultyLevel(1)}
+              >
+                Easy
+              </div>
+              <div
+                className={
+                  "difficulty-button " +
+                  (difficultyLevel === 2 ? "selected" : "")
+                }
+                onClick={() => setDifficultyLevel(2)}
+              >
+                Medium
+              </div>
+              <div
+                className={
+                  "difficulty-button " +
+                  (difficultyLevel === 3 ? "selected" : "")
+                }
+                onClick={() => setDifficultyLevel(3)}
+              >
+                Hard
+              </div>
+            </div>
+
+            <div
+              className="flowers-grid"
+              style={{
+                gridTemplateColumns: `repeat(${10 * difficultyLevel}, 25px)`,
+              }}
+            >
+              {listOfAllFlowers.map((flower, index) => {
+                const isTulip = randomPlacesForTulips.includes(index);
+                if (clickedFlowers.includes(index))
+                  return (
+                    <div key={index} className="single-flower-wrapper">
+                      💚
+                    </div>
+                  );
+                return (
+                  <div key={index} className="single-flower-wrapper">
+                    <img
+                      src={isTulip ? Tulip : arrayOfFlowers[flower]}
+                      onClick={() => isTulip && handleFlowerClick(index)}
+                      alt="flower"
+                    />
+                  </div>
+                );
+              })}
+            </div>
+
+            <div className="clicked-flowers">
+              {clickedFlowers.map((index) => (
+                <div key={index} className="taken">
+                  <img
+                    src={
+                      randomPlacesForTulips.includes(index)
+                        ? Tulip
+                        : arrayOfFlowers[listOfAllFlowers[index]]
+                    }
+                    alt="flower"
+                  />
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </>
+    </>
   );
 };
